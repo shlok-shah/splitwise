8dd96aa56f26fddf3f6cc5e4ad790b5d6271132c
Shlok Shah <55475572+shlok-shah@users.noreply.github.com>
2024-06-19 19:34:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// Define the number of people
 #define NUM_PEOPLE 3
 
-// Function to find the index of the minimum value in an array
 int findMinIndex(int arr[])
 {
     int minIdx = 0;
@@ -16,7 +14,6 @@ int findMinIndex(int arr[])
     return minIdx; // Return the index of the smallest value
 }
 
-// Function to find the index of the maximum value in an array
 int findMaxIndex(int arr[])
 {
     int maxIdx = 0;
@@ -25,33 +22,30 @@ int findMaxIndex(int arr[])
         if (arr[i] > arr[maxIdx])
             maxIdx = i;
     }
-    return maxIdx; // Return the index of the largest value
+    return maxIdx; 
 }
 
-// Function to find the smaller of two values
 int minOfTwo(int num1, int num2)
 {
     return (num1 < num2) ? num1 : num2;
 }
 
-// Function to minimize the cash flow among people represented by the amount array
+
 void settleAmounts(int amount[])
 {
     int maxCredit = findMaxIndex(amount), maxDebit = findMinIndex(amount);
 
-    // If both max credit and max debit are zero, all debts are settled
     if (amount[maxCredit] == 0 && amount[maxDebit] == 0)
         return;
 
-    // Find the minimum of the amounts to be transferred
     int minAmount = minOfTwo(-amount[maxDebit], amount[maxCredit]);
     amount[maxCredit] -= minAmount;
     amount[maxDebit] += minAmount;
 
-    // Display the transaction
+    
     cout << "Person " << maxDebit << " pays " << minAmount << " to Person " << maxCredit << "." << endl;
 
-    // Recursively settle the remaining amounts
+    
     settleAmounts(amount);
 }
 
